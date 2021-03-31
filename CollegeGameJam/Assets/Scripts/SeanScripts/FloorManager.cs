16ab00b540d5a3cbe8ce876ae78a04bf289510f5
BlastLad <blastlad@gmail.com>
2021-03-30 21:03:52 -0400

    More work on cinemachine camera and another test scene just to try out camera settings.

---
+++ CollegeGameJam/Assets/Scripts/SeanScripts/FloorManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FloorManager : MonoBehaviour
+{
+
+    public Transform[] floorLevelCameras;
+
+    [SerializeField]
+    GameObject cinemachineCameraObj;
+
+    GameObject playerObj;
+
+    [SerializeField]
+    int blockHeight = 5;
+    
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObj = PlayerCore.Instance.groundTarget.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerObj.transform.position.y - this.transform.position.y >= blockHeight)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + blockHeight, this.transform.position.x);
+        }
+        else if (playerObj.transform.position.y - this.transform.position.y <= -blockHeight)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - blockHeight, this.transform.position.x);
+        }
+    }
+
+
+}
---
+++ CollegeGameJam/Assets/Scripts/SeanScripts/PlayerCore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCore : MonoBehaviour
+{
+    public static PlayerCore Instance { get; private set; }
+
+    public Transform groundTarget;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("ERROR: Already a PlayerCore Instance is this scene");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
