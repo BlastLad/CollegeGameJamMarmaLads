a53e33451ba5c45c75b74933a4d46304063f352a
BlastLad <blastlad@gmail.com>
2021-03-31 18:22:44 -0400

    Added a new Player layer, Began work on SnowBall Pushing

---
+++ CollegeGameJam/Assets/Scripts/SeanScripts/CentralSnowBallManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralSnowBallManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    GameObject snowBallPrefab;
+
+    [SerializeField]
+    GameObject currentSnowBall;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateSnowballManager()//Something to be called by other scipts if the Thing is not initally live
+    {
+        if (currentSnowBall != null)
+        {
+            Destroy(currentSnowBall);
+        }
+        else
+        {
+            SpawnNewSnowBall();
+        }
+
+
+    }
+
+
+    public void SpawnNewSnowBall()
+    {
+        
+        currentSnowBall = Instantiate(snowBallPrefab, transform.position, Quaternion.identity);
+        currentSnowBall.transform.parent = transform;
+    }
+}
---
+++ CollegeGameJam/Assets/Scripts/SeanScripts/SnowBallMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBallMovement : MonoBehaviour
+{
+    [SerializeField]
+    float pushSpeed = 2f;
+    [SerializeField]
+    float rayLength = 5.5f;
+
+    [SerializeField]
+    LayerMask WallDetection;
+    [SerializeField]
+    LayerMask PlayerLayer;
+
+
+    Vector3 spawnPosition;
+
+    Vector3 startingPosition;
+    Vector3 rayFirePosition;
+    Vector3 targetCenterPosition;
+
+    bool snowBallMoving = false;
+    int unitToBlockRatio = 5;
+    bool canBeDestroyed = true;
+
+
+    string PlayerString = "Player";
+
+
+
+    private void Awake()
+    {
+        spawnPosition = transform.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        //Debug.DrawRay(rayFirePosition, Vector3.forward, Color.cyan);
+
+        if (snowBallMoving)
+        {
+            if (Vector3.Distance(startingPosition, transform.position) > unitToBlockRatio)
+            {
+                transform.position = targetCenterPosition;
+                snowBallMoving = false;
+                return;
+            }
+
+
+            transform.position += (targetCenterPosition - startingPosition) * pushSpeed * Time.deltaTime;
+            return;
+
+
+        }
+
+        if ((transform.position.y - spawnPosition.y) <= -unitToBlockRatio && canBeDestroyed == true)
+        {
+            DestroySnowBall();
+        }
+    }
+
+
+
+    public void DestroySnowBall()
+    {
+        GetComponentInParent<CentralSnowBallManager>().SpawnNewSnowBall();
+
+        Destroy(this.gameObject);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(PlayerString))
+        {
+            rayFirePosition = new Vector3(transform.position.x + 2.5f, transform.position.y + 2.5f, transform.position.z - 2.5f);
+
+            if (!Physics.Raycast(rayFirePosition, Vector3.forward, rayLength, WallDetection))//MIGHT NEED TO FIRE 2 RAYS DUE TO HALF BLOCKS BEING 2.5 high, WILL KNOW FOR SURE ONCE BUILT
+            {
+                targetCenterPosition = transform.position + (Vector3.forward * unitToBlockRatio);
+                startingPosition = transform.position;
+                snowBallMoving = true;
+            }
+         
+        }
+
+    }
+
+
+}
