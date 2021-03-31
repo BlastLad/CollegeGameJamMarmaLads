3f919e5295fc0839efbc8304893375925f562dbe
RossyRoo <rossesmusic@gmail.com>
2021-03-31 14:12:46 -0400

    EnemyAI / Projectiles / Snow
    I wrote the enemy AI and projectile script and also made a gentle snowfall particle effect that can be placed above levels where we any weather effect.

--- CollegeGameJam/Assets/Scripts/RossScripts/EnemyAI.cs
+++ CollegeGameJam/Assets/Scripts/RossScripts/EnemyAI.cs
@@ -1,25 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyAI : MonoBehaviour
 {
+    [Tooltip("Enter the string name of the scene in the build you want to load when enemy is touched.")]
+    [SerializeField] string nextSceneToLoad;
 
-    void Start()
+    private Transform targetTransform;  // Found in start method
+    [Tooltip("This changes the angle the projectile is thrown")]
+    [SerializeField]
+    float firingAngle = 45f;
+    [Tooltip("This changes the force of the throw")][SerializeField]
+    float gravity = 35f;
+    [Tooltip("This changes the delay of the next throw")][SerializeField]
+    float throwDelay = 4f;
+
+    [Tooltip("Enter the prefab of the projectile you want to fire")][SerializeField]
+    GameObject projectile;
+    [Tooltip("Enter the transform point from which the projectile is fired")][SerializeField]
+    Transform firingPoint;
+
+    private bool canFireProjectiles;     // This is set to false when the enemy is touched, so they can't throw during the victory animation
+
+    private void Start()
     {
-        
+        targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        canFireProjectiles = true;
+        StartCoroutine(LaunchProjectiles());
     }
 
-    void Update()
+    private void Update()
     {
-        
+        RotateToTarget();
+    }
+
+    IEnumerator LaunchProjectiles()
+    {
+        //A method to throw projectiles on a loop toward the target's transform
+
+        while(canFireProjectiles)
+        {
+            yield return new WaitForSeconds(throwDelay);
+
+            GameObject newProjectile = Instantiate(projectile, firingPoint.position, Quaternion.identity) as GameObject;
+
+            float targetDistance = Vector3.Distance(newProjectile.transform.position, targetTransform.position); // Calculate distance to player
+            float projectileVelocity = targetDistance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity); // Calculate velocity needed to hit target
+
+            //Extract the x and y component of the velocity
+            float velocityX = Mathf.Sqrt(projectileVelocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
+            float velocityY = Mathf.Sqrt(projectileVelocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
+
+            float flightDuration = targetDistance / velocityX;    // calculate flight time
+
+            newProjectile.transform.rotation = Quaternion.LookRotation(targetTransform.position - newProjectile.transform.position); // Get rotation
+
+            float elapseTime = 0f;
+
+            while (elapseTime < flightDuration)
+            {
+                newProjectile.transform.Translate(0, (velocityY - (gravity * elapseTime)) * Time.deltaTime, velocityX * Time.deltaTime);
+
+                elapseTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 lookPoint = new Vector3
+                    (targetTransform.position.x, transform.position.y, targetTransform.position.z);
+        transform.LookAt(lookPoint);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // When the player comes within the enemy's trigger radius, we will trigger LevelComplete()
+
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("LEVEL END");
+            StartCoroutine(LevelComplete());
         }
+    }
+
+    IEnumerator LevelComplete()
+    {
+        canFireProjectiles = false;
+        float levelCompleteBuffer = 1.5f;       // Must be enough time to play animations and SFX
+
+        // ADD --- Play victory animations and sound effects
+        yield return new WaitForSeconds(levelCompleteBuffer);
+        SceneManager.LoadScene(nextSceneToLoad);
     }
 }
---
+++ CollegeGameJam/Assets/Scripts/RossScripts/Projectile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem projectileCollisionVFX;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // No matter what the projectile collides with, it explodes into a particle effect
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<SphereCollider>().enabled = false;     //Turn off the trigger collider
+
+        projectileCollisionVFX.Play();
+
+        if (other.gameObject.tag == "Enemy") { return; }
+
+        else if (other.gameObject.tag == "Player")
+        {
+            // When the projectile's box collider touches the player, we begin a coroutine play
+            // A player animation and shortly reload the scene
+            StartCoroutine(ReloadLevel(other.gameObject));
+        }
+
+        else
+        {
+            StartCoroutine(DestroyProjectile()); // this disables the projectile's trigger and then destroys
+        }
+    }
+
+
+    IEnumerator DestroyProjectile()
+    {
+        // If the projectile hits anything besides the player, its trigger will turn off
+        // And after the duration of the particle animation, it will destroy itself
+        float timeTilDestroy = 1f;
+
+        yield return new WaitForSeconds(timeTilDestroy); 
+        Destroy(gameObject);
+    }
+
+    IEnumerator ReloadLevel(GameObject player)
+    {
+        float levelReloadBuffer = 1.5f;   // This is temporary - I would like to wait for the duration of player's "Get hit" animation
+        // ADD --- PLAY "GET HIT" ANIMATION
+        // ADD --- DISABLE PLAYER CONTROLS
+        yield return new WaitForSeconds(levelReloadBuffer);     //We can make this the duration of the player's "Get hit" animation
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
